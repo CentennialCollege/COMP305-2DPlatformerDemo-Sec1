b1bfd6e1e2b197f816c867127bc73d9e28dcd428
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-10-19 19:31:06 -0400

    added coin prefab

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	//PRIVATE INSTANCE VARIABLES ++++++++++++++++++++++++++++++
+	private AudioSource[] _audioSources;
+	private AudioSource _coinSound;
+
+	// Use this for initialization
+	void Start () {
+		this._audioSources = gameObject.GetComponents<AudioSource> ();
+		this._coinSound = this._audioSources [0];
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D otherCollider) {
+		if (otherCollider.gameObject.CompareTag ("Coin")) {
+			this._coinSound.Play();
+		}
+	}
+}
