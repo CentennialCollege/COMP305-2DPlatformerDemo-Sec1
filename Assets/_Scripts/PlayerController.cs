4eb5b0331c85263077423deb8fad42d6fb04f91e
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-10-20 17:39:46 -0400

    completed player prefab

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -29,8 +29,11 @@ public class PlayerController : MonoBehaviour {
 	private Animator _animator; // we'll use this later
 	private AudioSource[] _audioSources;
 	private AudioSource _coinSound;
+	private AudioSource _jumpSound;
 
-	private float _moving = 0;
+	private float _movingValue = 0;
+	private bool _isFacingRight = true;
+	private bool _isGrounded = true;
 
 	// Use this for initialization
 	void Start () {
@@ -40,6 +43,7 @@ public class PlayerController : MonoBehaviour {
 
 		this._audioSources = gameObject.GetComponents<AudioSource> ();
 		this._coinSound = this._audioSources [0];
+		this._jumpSound = this._audioSources [1];
 	}
 
 	void FixedUpdate () {
@@ -49,25 +53,47 @@ public class PlayerController : MonoBehaviour {
 		float absVelX = Mathf.Abs (this._rigidBody2D.velocity.x);
 		float absVelY = Mathf.Abs (this._rigidBody2D.velocity.y);
 
-		this._moving = Input.GetAxis ("Horizontal"); // value is between -1 and 1
+		this._movingValue = Input.GetAxis ("Horizontal"); // value is between -1 and 1
 
-		if (this._moving != 0) {
+		// check if player is moving
+
+		if (this._movingValue != 0) {
 			// we're moving
 			this._animator.SetInteger("AnimState", 1); // play walk clip
-			if(this._moving > 0) {
+			if(this._movingValue > 0) {
 				// moving right
 				if(absVelX < this.velocityRange.vMax) {
 					forceX = this.speed;
+					this._isFacingRight = true;
+					this._flip();
 				}
-			} else if (this._moving < 0) {
+			} else if (this._movingValue < 0) {
 				// moving left
 				if(absVelX < this.velocityRange.vMax) {
 					forceX = -this.speed;
+					this._isFacingRight = false;
+					this._flip();
 				}
 			}
-		} else if (this._moving == 0) {
+		} else if (this._movingValue == 0) {
 			// we're idle
 			this._animator.SetInteger("AnimState", 0);
+		}
+
+		// Check if player is jumping
+
+		if((Input.GetKey("up") || Input.GetKey(KeyCode.W))) {
+			// check if the player is grounded
+			if(this._isGrounded) {
+				// player is jumping
+				this._animator.SetInteger("AnimState", 2);
+				if(absVelY < this.velocityRange.vMax) {
+					forceY = this.jump;
+					this._jumpSound.Play();
+					this._isGrounded = false;
+				}
+			}
+
 		}
 
 		this._rigidBody2D.AddForce (new Vector2 (forceX, forceY));
@@ -78,4 +104,21 @@ public class PlayerController : MonoBehaviour {
 			this._coinSound.Play();
 		}
 	}
+
+	void OnCollisionStay2D(Collision2D otherCollider) {
+		if (otherCollider.gameObject.CompareTag ("Platform")) {
+			this._isGrounded = true;
+		}
+	}
+
+
+	// PRIVATE METHODS +++++++++++++++++++++++++++++++++++++++
+	private void _flip() {
+		if (this._isFacingRight) {
+			this._transform.localScale = new Vector3(1f, 1f, 1f); // flip back to normal
+		} else {
+			this._transform.localScale = new Vector3(-1f, 1f, 1f);
+		}
+	}
+
 }
